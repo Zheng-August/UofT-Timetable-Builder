f23b56d87195f449825778deb1deaa4779beef07
AugustZ <zhengzhongze4@gmail.com>
2023-12-20 23:44:22 -0500

    fetch_special_sections

--- fetch_data.py
+++ fetch_data.py
@@ -24,6 +24,37 @@ def fetch_meeting_sections(conn, course_code):
     cursor.execute('SELECT * FROM meeting_sections WHERE course_code = ?', (course_code,))
     return cursor.fetchall()
 
+def fetch_key_sections(conn, course_key):
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM courses WHERE course_code LIKE ? OR name LIKE ?"
+    param = ('%' + course_key + '%','%' + course_key + '%')
+    cursor.execute(query, param)
+    courses = cursor.fetchall()
+
+    course_list = []
+    for course in courses:
+        course_list.append(' '.join(course[2:5]))
+    return course_list
+
+
+def fetch_special_sections(conn, course_code, section_code):
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM courses WHERE course_code = ? AND section_code = ?"
+    param = (course_code,section_code)
+    cursor.execute(query, param)
+    course = cursor.fetchall()
+
+    course_id = course[0][1] # get id
+    cursor.execute('SELECT * FROM meeting_sections WHERE course_id = ?', (course_id,))
+    meeting_times = cursor.fetchall()
+
+    return meeting_times
+
+    
+
+
 def main():
     db_path = 'courses.db'
     conn = connect_to_database(db_path)
@@ -33,13 +64,22 @@ def main():
         print(course)
     """
     
-    course_code = 'ISP100H5'
-    course_details = fetch_course_details(conn, course_code)
-    print("\nCourse Details:", course_details)
+    # course_code = 'ISP100H5'
+    # course_details = fetch_course_details(conn, course_code)
+    # print("\nCourse Details:", course_details)
 
-    meeting_sections = fetch_meeting_sections(conn, course_code)
-    print("\nMeeting Sections:")
-    print(meeting_sections)
+    # meeting_sections = fetch_meeting_sections(conn, course_code)
+    # print("\nMeeting Sections:")
+    # print(meeting_sections)
+    
+    # course_key = 'csc108H1'
+    # course_details = fetch_key_sections(conn, course_key)
+    # print("\nCourse Details:", course_details)
+
+    course_code = 'CSC108H1'
+    section_code = 'F'
+    course_details = fetch_special_sections(conn, course_code, section_code)
+    print(course_details)
 
 
 if __name__ == '__main__':

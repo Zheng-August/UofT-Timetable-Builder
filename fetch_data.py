d5dd293d7df4627d080982c310729a0cb95f4352
AugustZ <zhengzhongze4@gmail.com>
2023-12-21 14:43:25 -0500

    backtracking

--- fetch_data.py
+++ fetch_data.py
@@ -1,40 +1,47 @@
 import sqlite3
 import json
 
+
 # connect to db
 def connect_to_database(db_path):
     conn = sqlite3.connect(db_path)
     return conn
 
+
 # get all corses
 def fetch_all_courses(conn):
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM courses')
+    cursor.execute("SELECT * FROM courses")
     return cursor.fetchall()
 
-# get spe corse 
+
+# get spe corse
 def fetch_course_details(conn, course_code):
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM courses WHERE course_code = ?', (course_code,))
+    cursor.execute("SELECT * FROM courses WHERE course_code = ?", (course_code,))
     return cursor.fetchone()
 
-# get some spe corse 's all meeting time 
+
+# get some spe corse 's all meeting time
 def fetch_meeting_sections(conn, course_code):
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM meeting_sections WHERE course_code = ?', (course_code,))
+    cursor.execute(
+        "SELECT * FROM meeting_sections WHERE course_code = ?", (course_code,)
+    )
     return cursor.fetchall()
+
 
 def fetch_key_sections(conn, course_key):
     cursor = conn.cursor()
 
     query = "SELECT * FROM courses WHERE course_code LIKE ? OR name LIKE ?"
-    param = ('%' + course_key + '%','%' + course_key + '%')
+    param = ("%" + course_key + "%", "%" + course_key + "%")
     cursor.execute(query, param)
     courses = cursor.fetchall()
 
     course_list = []
     for course in courses:
-        course_list.append(' '.join(course[2:5]))
+        course_list.append(" ".join(course[2:5]))
     return course_list
 
 
@@ -42,28 +49,26 @@ def fetch_special_sections(conn, course_code, section_code):
     cursor = conn.cursor()
 
     query = "SELECT * FROM courses WHERE course_code = ? AND section_code = ?"
-    param = (course_code,section_code)
+    param = (course_code, section_code)
     cursor.execute(query, param)
     course = cursor.fetchall()
 
-    course_id = course[0][1] # get id
-    cursor.execute('SELECT * FROM meeting_sections WHERE course_id = ?', (course_id,))
+    course_id = course[0][1]  # get id
+    cursor.execute("SELECT * FROM meeting_sections WHERE course_id = ?", (course_id,))
     meeting_times = cursor.fetchall()
 
     return meeting_times
 
-    
-
 
 def main():
-    db_path = 'courses.db'
+    db_path = "courses.db"
     conn = connect_to_database(db_path)
-    
+
     """courses = fetch_all_courses(conn)
     for course in courses:
         print(course)
     """
-    
+
     # course_code = 'ISP100H5'
     # course_details = fetch_course_details(conn, course_code)
     # print("\nCourse Details:", course_details)
@@ -71,16 +76,18 @@ def main():
     # meeting_sections = fetch_meeting_sections(conn, course_code)
     # print("\nMeeting Sections:")
     # print(meeting_sections)
-    
+
     # course_key = 'csc108H1'
     # course_details = fetch_key_sections(conn, course_key)
     # print("\nCourse Details:", course_details)
+    
+    
 
-    course_code = 'CSC108H1'
-    section_code = 'F'
+    course_code = "CSC108H1"
+    section_code = "F"
     course_details = fetch_special_sections(conn, course_code, section_code)
     print(course_details)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,112 +1,102 @@
-[
-    (
-        3797,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0101",
-        "Tovi Grossman",
-        '[{"day": 1, "start": 43200000, "end": 46800000, "location": "AH"}, {"day": 3, "start": 39600000, "end": 46800000, "location": "AH"}]',
-        196,
-        158,
-        '"INPER"',
-    ),
-    (
-        3798,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC5101",
-        "Sadia Sharmin",
-        '[{"day": 1, "start": 64800000, "end": 68400000, "location": "ON"}, {"day": 3, "start": 64800000, "end": 72000000, "location": "ON"}]',
-        200,
-        162,
-        '"HYBR"',
-    ),
-    (
-        3799,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0301",
-        "Joonho Jonathan Kim",
-        '[{"day": 3, "start": 46800000, "end": 54000000, "location": "SF"}, {"day": 1, "start": 50400000, "end": 54000000, "location": "MP"}]',
-        196,
-        161,
-        '"INPER"',
-    ),
-    (
-        3800,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0502",
-        "Jacqueline Smith",
-        '[{"day": 2, "start": 32400000, "end": 39600000, "location": "MY"}, {"day": 4, "start": 32400000, "end": 36000000, "location": "MY"}]',
-        234,
-        198,
-        '"INPER"',
-    ),
-    (
-        3801,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0501",
-        "Jacqueline Smith",
-        '[{"day": 2, "start": 32400000, "end": 39600000, "location": "MY"}, {"day": 4, "start": 32400000, "end": 36000000, "location": "MY"}]',
-        234,
-        200,
-        '"INPER"',
-    ),
-    (
-        3802,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0601",
-        "Angela M. Zavaleta Bernuy",
-        '[{"day": 2, "start": 39600000, "end": 46800000, "location": "HS"}, {"day": 4, "start": 43200000, "end": 46800000, "location": "HS"}]',
-        196,
-        169,
-        '"INPER"',
-    ),
-    (
-        3803,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0201",
-        "Mohi Reza",
-        '[{"day": 1, "start": 46800000, "end": 50400000, "location": "FE"}, {"day": 3, "start": 46800000, "end": 54000000, "location": "FE"}]',
-        196,
-        157,
-        '"INPER"',
-    ),
-    (
-        3804,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0401",
-        "Tovi Grossman",
-        '[{"day": 1, "start": 54000000, "end": 57600000, "location": "MP"}, {"day": 3, "start": 54000000, "end": 61200000, "location": "MP"}]',
-        196,
-        167,
-        '"INPER"',
-    ),
-    (
-        3805,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0701",
-        "Fernando Yanez",
-        '[{"day": 2, "start": 46800000, "end": 54000000, "location": "MP"}, {"day": 4, "start": 46800000, "end": 50400000, "location": "MP"}]',
-        196,
-        168,
-        '"INPER"',
-    ),
-    (
-        3806,
-        "6445bd58c1e5fe0004158958",
-        "CSC108H1",
-        "LEC0801",
-        "Angela M. Zavaleta Bernuy",
-        '[{"day": 2, "start": 54000000, "end": 61200000, "location": "MC"}, {"day": 4, "start": 54000000, "end": 57600000, "location": "MC"}]',
-        196,
-        176,
-        '"INPER"',
-    ),
-]
+import sqlite3
+import json
+
+
+def fetch_special_sections(conn, course_code, section_code):
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM courses WHERE course_code = ? AND section_code = ?"
+    param = (course_code, section_code)
+    cursor.execute(query, param)
+    course = cursor.fetchall()
+
+    course_id = course[0][1]  # get id
+    cursor.execute("SELECT * FROM meeting_sections WHERE course_id = ?", (course_id,))
+    meeting_times = cursor.fetchall()
+
+    return meeting_times
+
+
+def connect_to_database(db_path):
+    conn = sqlite3.connect(db_path)
+    return conn
+
+
+def can_schedule(timetable, time_slot):
+    # 是否冲突
+    for scheduled_slot in timetable:
+        if scheduled_slot["time"]["day"] == time_slot["day"] and not (
+            scheduled_slot["time"]["end"] <= time_slot["start"]
+            or scheduled_slot["time"]["start"] >= time_slot["end"]
+        ):
+            return False
+    return True
+
+
+def generate_timetable(courses, timetable, course_keys, course_index=0, type_index=0):
+    if course_index == len(course_keys):
+        return True  # 所有的课程的所有课程类型已排列完成
+
+    course_key = course_keys[course_index]
+    course_types = list(courses[course_key].keys())
+
+    if type_index == len(course_types):
+        # 当前课程的所有类型已经安排完成
+        return generate_timetable(courses, timetable, course_keys, course_index + 1, 0)
+
+    course_type = course_types[type_index]
+    sections = courses[course_key][course_type]
+
+    for section, time_slots in sections.items():
+        for time_slot in time_slots:
+            if can_schedule(timetable, time_slot):
+                # 如果没有冲突则安排课程表
+                timetable_entry = {
+                    "course": course_key,
+                    "section": section,
+                    "time": time_slot,
+                }
+                timetable.append(timetable_entry)
+                if generate_timetable(
+                    courses, timetable, course_keys, course_index, type_index + 1
+                ):
+                    return True
+                timetable.pop()  # 回溯
+
+    return False  # 寻找失败
+
+
+def main():
+    db_path = "courses.db"
+    conn = connect_to_database(db_path)
+
+    course_code_lst = ["CSC108H1", "CHM136H1", "BIO130H1", "CSC165H1", "MAT136H1","STA130H1"]
+    section_code = "S"
+    courses_example = {}
+
+    for course_code in course_code_lst:
+        sections = fetch_special_sections(conn, course_code, section_code)
+        courses_example[course_code] = {}
+
+        for section in sections:
+            section_type = section[3][:3]
+            data_list = json.loads(section[5])
+            if section_type in courses_example[course_code]:
+                courses_example[course_code][section_type][section[3]] = data_list
+            else:
+                courses_example[course_code][section_type] = {}
+                courses_example[course_code][section_type][section[3]] = data_list
+
+    # print(courses_example)
+    timetable = []
+    course_keys = list(courses_example.keys())
+
+    # 生成课程表
+    if generate_timetable(courses_example, timetable, course_keys):
+        print("成功生成课程表:", timetable)
+    else:
+        print("没有符合条件的课程表")
+
+
+if __name__ == "__main__":
+    main()
